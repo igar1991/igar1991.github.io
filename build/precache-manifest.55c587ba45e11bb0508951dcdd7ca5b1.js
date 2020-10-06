863d4c8aab77560ec76a114d8283b70ebe8b5f53
iiggaarr <iiggaarr19912004@gmail.com>
2020-10-06 11:13:43 +0300

    folder

---
+++ build/precache-manifest.55c587ba45e11bb0508951dcdd7ca5b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9887bd2e642201170142b9114e260930",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2ce8fff9492b0ec2070a",
+    "url": "./static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "ccfa9d511f3cde6ec75c",
+    "url": "./static/css/main.3ee947cd.chunk.css"
+  },
+  {
+    "revision": "2ce8fff9492b0ec2070a",
+    "url": "./static/js/2.4470c2da.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.4470c2da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccfa9d511f3cde6ec75c",
+    "url": "./static/js/main.985d4e23.chunk.js"
+  },
+  {
+    "revision": "82a8dc7823b3638c0eb7",
+    "url": "./static/js/runtime-main.18f35c0e.js"
+  }
+]);
